1e99529f0e333fe2661547ec91ca5ed664371899
Olguca2020 <121376394+Olguca2020@users.noreply.github.com>
2023-05-15 06:58:17 +0300

    ht1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+const cardsContainer = document.querySelector(`.gallery`);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+cardsContainer.insertAdjacentHTML(`beforeend`, galleryMarkup);
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`;
+    })
+    .join('');
+}
 
-console.log(galleryItems);
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox', function () {});
