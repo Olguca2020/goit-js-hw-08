09d4e43559637ab07fe00eb02b6eec801fa36bed
Olguca2020 <121376394+Olguca2020@users.noreply.github.com>
2023-05-20 20:02:56 +0300

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -45,4 +45,5 @@ window.addEventListener('load', function () {
         }
       });
   }
+  localStorage.clear();
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+const refs = {
+  form: document.querySelector(`.feedback-form`),
+  inputEmail: document.querySelector(`input[type="email"]`),
+  textarea: document.querySelector(`textarea[name="message"]`),
+  btnSubmit: document.querySelector(`button[type="submit"]`),
+};
 
+refs.textarea.addEventListener(`input`, throttle(onTextareaInput, 500));
+refs.inputEmail.addEventListener(`input`, onEmailInput);
+// refs.form.addEventListener(`submit`, onFormSubmit);
+
+function onTextareaInput(e) {
+  const message = e.currentTarget.value;
+  console.log(message);
+}
+function onEmailInput(e) {
+  const email = e.currentTarget.value;
+  return email;
+}
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+export default {
+  save,
+  load,
+};
