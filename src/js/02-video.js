7422217c4551e8ee96fe3034e2104ab49f3ec6ef
Olguca2020 <121376394+Olguca2020@users.noreply.github.com>
2023-05-20 10:03:50 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const currentTime = data.seconds;
+    const currentTimeObj = { currentTime };
+
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(currentTimeObj)
+    );
+  }, 1000)
+);
+
+window.addEventListener('load', function () {
+  const currentTimeJSON = localStorage.getItem('videoplayer-current-time');
+  if (currentTimeJSON) {
+    const currentTimeObj = JSON.parse(currentTimeJSON);
+    const savedCurrentTime = currentTimeObj.currentTime;
+    player
+      .setCurrentTime(savedCurrentTime)
+      .then(function (seconds) {
+        console.log('Playback position restored:', seconds);
+      })
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            console.log('Invalid time:', savedCurrentTime);
+            break;
+          default:
+            console.log('Error:', error);
+            break;
+        }
+      });
+  }
+});
